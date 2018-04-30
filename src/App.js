9978ea2bf7bc5737318736d9a336dbe0d367ca50
Brandon Fizer <30492636+Track7Dev@users.noreply.github.com>
2018-04-30 03:23:09 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -87,7 +87,7 @@ class App extends Component {
 
   componentDidMount(){
     this.systemConnect();
-    console.log("---->",process.env.SERVER);
+    console.log("---->",process.env.SE);
     
   }
 
