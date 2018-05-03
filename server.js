95e007fa63044ff5f9d1d7209808bbf1299b46f3
Brandon Fizer <30492636+Track7Dev@users.noreply.github.com>
2018-05-03 02:22:45 -0400

    server

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const path = require('path');
+const server = express();
+const {display, log} = require('./src/tools');
+const port = process.env.PORT || 3000;
+
+server.use(express.static(path.join(__dirname, 'build')));
+
+server.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+server.listen(port, (err) => !err ? display(`RUNNING PRODUCTION ENVIORNMENT`) : display('ERROR CONNECTING'));
--- src/tools.js
+++ src/tools.js
@@ -1,5 +1,7 @@
 const log = message => process.env.NODE_ENV === 'development' ? console.log(message) : null;
+const display = message => process.env.NODE_ENV === 'production' ? console.log(message) : null;
 
 module.exports = {
-  log
+  log,
+  display
 }
