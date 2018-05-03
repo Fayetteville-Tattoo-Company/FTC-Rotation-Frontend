0eb9ef372bbeacee00a5c3912a76f2daabb39731
Brandon Fizer <30492636+Track7Dev@users.noreply.github.com>
2018-05-03 02:29:03 -0400

    server

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const path = require('path');
 const server = express();
+const cors = require('cors');
 const {display, log} = require('./src/tools');
 const port = process.env.PORT || 3000;
 
+server.use(cors());
 server.use(express.static(path.join(__dirname, 'build')));
 
 server.get('/', function (req, res) {
